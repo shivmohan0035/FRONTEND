50a31822a225276fec31775ab04600b9c9b81f00
shivmohan0035 <princekumarchaurasia825@gmail.com>
2026-07-08 12:22:37 +0530

    Conditional Rendering

--- Map_method/src/App.jsx
+++ Map_method/src/App.jsx
@@ -3,10 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 function App() {
+  // let foodItems = [];
   let foodItems=[ "Pizza", "Burger", "Pasta", "Salad", "Sushi"];
+
+// (1) Using if-else statement
+  // if(foodItems.length===0){
+  //   return <h1>No Food Items Available</h1>
+  // }
   return (
     <>
       <h1>Food Items</h1>
+      {/* (2) Using Ternary Operator ---> agar return ke andhar ==> null, empty,undefined hoga to ye line ignore ho jayegi by JSX */}
+      
+      {foodItems.length===0 ? <h1>No Food Items Available</h1> : null}
+
        <ul className="list-group">
         {foodItems.map(item=>
           <li key={item} className="list-group-item">
